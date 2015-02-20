d19a93b96cb2d0d540fbfe32114fe0ebe53897aa
Guillaume Claret <dev@clarus.me>
2015-02-20 18:01:40 +0100

    Comments

--- Main.v
+++ Main.v
@@ -6,9 +6,11 @@ Require Import ListString.All.
 Import ListNotations.
 Import C.Notations.
 
+(** The classic Hello World program. *)
 Definition hello_world : C.t Unix.effects unit :=
   Unix.log (LString.s "Hello world!").
 
+(** Ask for the user name and answer hello. *)
 Definition your_name : C.t Unix.effects unit :=
   do! Unix.log (LString.s "What is your name?") in
   let! name := Unix.read_line in
@@ -17,20 +19,26 @@ Definition your_name : C.t Unix.effects unit :=
   | Some name => Unix.log (LString.s "Hello " ++ name ++ LString.s "!")
   end.
 
-Definition main := Extraction.Lwt.run (Extraction.eval your_name).
+(** Extract the Hello World program to `extraction/main.ml`. Run the `Makefile`
+    in `extraction/` to compile it. *)
+Definition main := Extraction.Lwt.run (Extraction.eval hello_world).
 Extraction "extraction/main" main.
 
+(** Specifications. *)
 Module Run.
+  (** The Hello World program only says hello. *)
   Definition hello_world_ok : Run.t hello_world tt.
     apply (Run.log_ok (LString.s "Hello world!")).
   Defined.
 
+  (** The Your Name program answers something when you give your name. *)
   Definition your_name_ok (name : LString.t) : Run.t your_name tt.
     apply (Run.Let (Run.log_ok _)).
     apply (Run.Let (Run.read_line_ok name)).
     apply (Run.log_ok _).
   Defined.
 
+  (** The Your Name program does nothing more in case of error on stdin. *)
   Definition your_name_error : Run.t your_name tt.
     apply (Run.Let (Run.log_ok _)).
     apply (Run.Let Run.read_line_error).
