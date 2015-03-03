19890469f5622e22f80dcdc54913f7ea9b4d2f1a
Guillaume Claret <dev@clarus.me>
2015-03-03 20:59:37 +0100

    Update to the new dependencies

--- Main.v
+++ Main.v
@@ -1,45 +1,45 @@
 Require Import Coq.Lists.List.
-Require Import IoEffects.All.
-Require Import IoEffectsUnix.All.
+Require Import Io.All.
+Require Import Io.System.All.
 Require Import ListString.All.
 
 Import ListNotations.
 Import C.Notations.
 
 (** The classic Hello World program. *)
-Definition hello_world : C.t Unix.effects unit :=
-  Unix.log (LString.s "Hello world!").
+Definition hello_world (argv : list LString.t) : C.t System.effects unit :=
+  System.log (LString.s "Hello world!").
 
 (** Ask for the user name and answer hello. *)
-Definition your_name : C.t Unix.effects unit :=
-  do! Unix.log (LString.s "What is your name?") in
-  let! name := Unix.read_line in
+Definition your_name (argv : list LString.t) : C.t System.effects unit :=
+  do! System.log (LString.s "What is your name?") in
+  let! name := System.read_line in
   match name with
   | None => ret tt
-  | Some name => Unix.log (LString.s "Hello " ++ name ++ LString.s "!")
+  | Some name => System.log (LString.s "Hello " ++ name ++ LString.s "!")
   end.
 
 (** Extract the Hello World program to `extraction/main.ml`. Run the `Makefile`
     in `extraction/` to compile it. *)
-Definition main := Extraction.Lwt.run (Extraction.eval hello_world).
+Definition main := Extraction.run hello_world.
 Extraction "extraction/main" main.
 
 (** Specifications. *)
 Module Run.
   (** The Hello World program only says hello. *)
-  Definition hello_world_ok : Run.t hello_world tt.
+  Definition hello_world_ok : Run.t (hello_world []) tt.
     apply (Run.log_ok (LString.s "Hello world!")).
   Defined.
 
   (** The Your Name program answers something when you give your name. *)
-  Definition your_name_ok (name : LString.t) : Run.t your_name tt.
+  Definition your_name_ok (name : LString.t) : Run.t (your_name []) tt.
     apply (Run.Let (Run.log_ok _)).
     apply (Run.Let (Run.read_line_ok name)).
     apply (Run.log_ok _).
   Defined.
 
   (** The Your Name program does nothing more in case of error on stdin. *)
-  Definition your_name_error : Run.t your_name tt.
+  Definition your_name_error : Run.t (your_name []) tt.
     apply (Run.Let (Run.log_ok _)).
     apply (Run.Let Run.read_line_error).
     apply Run.Ret.
